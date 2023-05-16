2b0d2c625f193de53eb605563c03c09b639a4f84
Andrey Litvinov <andreoyy.litvinovv@gmail.com>
2023-05-16 16:53:57 +0300

    Icon added

--- Clock/Clock.Designer.cs
+++ Clock/Clock.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Text = "Clock";
         }
 
         #endregion
